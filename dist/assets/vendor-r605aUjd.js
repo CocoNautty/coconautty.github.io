87b582242377c915fa58a591b7233ea9a9c6bb4f
CocoNautty <coconautty@gmail.com>
2024-11-24 02:16:09 -0500

    fixed buggy onScroll()

--- dist/assets/vendor-r605aUjd.js
+++ dist/assets/vendor-r605aUjd.js
@@ -1,4 +1,4 @@
-import{g as oc}from"./index-l11ugrqi.js";var Ko={exports:{}},nl={},Yo={exports:{}},T={};/**
+import{g as oc}from"./index-BrVYLgZJ.js";var Ko={exports:{}},nl={},Yo={exports:{}},T={};/**
  * @license React
  * react.production.min.js
  *
