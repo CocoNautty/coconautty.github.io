a09a99093bfed5c8d89bd699868e52a2d8b7186a
CocoNautty <coconautty@gmail.com>
2024-11-24 22:10:05 -0500

    attempt to fix behavior on safari

--- dist/assets/vendor-8VHiC26Y.js
+++ dist/assets/vendor-8VHiC26Y.js
@@ -1,4 +1,4 @@
-import{g as yx,c as Zi}from"./index-7ODGB91M.js";var w0={exports:{}},zo={},S0={exports:{}},ue={};/**
+import{g as yx,c as Zi}from"./index-FIRG3sZU.js";var w0={exports:{}},zo={},S0={exports:{}},ue={};/**
  * @license React
  * react.production.min.js
  *
